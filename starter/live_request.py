cf7b8cec21cd2ace70d2327be15b5101959e9a5b
Hau Pham <haupham2539@gmail.com>
2023-10-09 12:50:35 +0700

    Add code for live request to sender

---
+++ starter/live_request.py
@@ -0,0 +1,38 @@
+"""
+Test the live prediction endpoint on Render
+"""
+import requests
+import logging
+
+logging.basicConfig(filename='./test_render.log',
+                    level=logging.INFO, format="%(asctime)-15s %(message)s")
+
+
+sample = {
+    'age': 27,
+    'workclass': 'Private',
+    'fnlgt': 201872,
+    'education': 'Some-college',
+    'education_num': 10,
+    'marital_status': 'Married-civ-spouse',
+    'occupation': 'Sales',
+    'relationship': 'Husband',
+    'race': 'White',
+    'sex': 'Male',
+    'capital_gain': 0,
+    'capital_loss': 0,
+    'hours_per_week': 50,
+    'native_country': 'United-States',
+    'salary': '<=50K'
+}
+
+
+app_url = "https://udacity.onrender.com/predict"
+
+r = requests.post(app_url, json=sample)
+print(r.status_code)
+print(r.json())
+
+logging.info("Testing live request")
+logging.info(f"Status code: {r.status_code}")
+logging.info(f"Response body: {r.json()}")
